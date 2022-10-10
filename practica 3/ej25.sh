e083cd906ad3cbfc8b325224f6323d3eb7c9b6ce
Emilia Romero <88845057+emrome@users.noreply.github.com>
2022-10-10 20:44:18 -0300

    Correccion ej25

--- practica 3/ej25.sh
+++ practica 3/ej25.sh
@@ -6,6 +6,10 @@
 # "-l": Devuelve longitud del arreglo
 # "-i": Imprime todos los elementos del arreglo en pantalla
 
+if [ $# -eq 0 ];then
+    echo "Debe enviar por lo menos una opcion (-b n, -l, -i)"
+    exit 2
+fi
 if [ $# -gt 2 ];then
     echo "Como maximo se deben enviar 2 parametros (-d n)"
     exit 1
@@ -16,9 +20,8 @@ users=($(cat /etc/group | grep users | cut -d: -f4 | tr "," " "))
 
 # Ejecuto según parametro
 case $1 in 
-        "-d")
-            if [ $2 -ge ${#users[*]} ] || [ $2 -lt 0 ];then 
-            # No existe esa posicion
+        "-b")
+            if [[ ! $2 =~ ^[0-9]+$ ]] ||[ $2 -ge ${#users[*]} ] || [ $2 -lt 0 ] ;then # No existe esa posicion
                 echo "Valor de n invalido"                
             else
                 echo ${users[$2]}
